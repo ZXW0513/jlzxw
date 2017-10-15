7ec39053127536855a363358526282f39d3843e5
ZXW513 <15202284246@163.com>
2017-10-15 21:26:33 +0800

    修改

--- dailyfresh/dailyfresh/urls.py
+++ dailyfresh/dailyfresh/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('df_user.jlgg'))
+    url(r'^', include('df_user.jlgg')),
     url(r'^',include('df_goods.urls')),
     url(r'^llllll',include('asdasd a')),
+    url(r'^zzzzzz',include('asdasd a')),
 ]
